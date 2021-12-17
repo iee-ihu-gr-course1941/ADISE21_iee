6c82e252bd2225167c2ceed8b448e0a34024a3a8
Xarhsvr <Xarhs.vrahas@gmail.com>
2021-12-18 01:01:41 +0200

    Update index.php

--- game/index.php
+++ game/index.php
@@ -1,1 +1,17 @@
-<?php echo "Hello" ?>
+<?php 
+
+require_once "../lib/db_connect.php";
+
+$sql = "SELECT id, username FROM users";
+$result = mysqli_query($mysqli, $sql);
+
+if(mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"] . " - username: " . $row["username"] . "<br>";
+    }
+}
+else {
+    echo "0 results";
+}
+
+?>
