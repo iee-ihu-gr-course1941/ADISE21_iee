cbf0f05d04eb043ea68a07b198c6ccdb1722a652
Xarhsvr <Xarhs.vrahas@gmail.com>
2021-12-20 16:35:21 +0200

    Update moutzouris.php

--- game/moutzouris.php
+++ game/moutzouris.php
@@ -4,7 +4,7 @@ require_once "../lib/db_connect.php";
 require_once "../lib/cards.php";
 
 $method = $_SERVER['REQUEST_METHOD'];
-$request = explode('/', trim($_SERVER['PATH_INFO']), '/');
+$request = explode('/', trim($_SERVER['PATH_INFO'], '/'));
 $input = json_decode(file_get_contents('php://input'), true);
 
 switch($r = array_shift($request)) {
