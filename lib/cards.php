6b2321832c0c1f0476b402d4f7a83695a9d2cd53
Xarhsvr <Xarhs.vrahas@gmail.com>
2021-12-20 16:31:21 +0200

    Create cards.php

---
+++ lib/cards.php
@@ -0,0 +1,86 @@
+<?php
+
+function show_cards() {
+    global $mysqli;
+
+    $sql = "SELECT x, y, player FROM cards_players";
+    $st = $mysqli -> prepare($sql);
+
+    $st -> execute();
+    $res = $st -> get_result();
+
+    header('Content-type: application/json');
+    print json_encode($res -> fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+}
+
+function reset_cards() {
+    $cards = array("A" => array("H", "B", "T", "R"),
+                   "2" => array("H", "B", "T", "R"),
+                   "3" => array("H", "B", "T", "R"),
+                   "4" => array("H", "B", "T", "R"),
+                   "5" => array("H", "B", "T", "R"),
+                   "6" => array("H", "B", "T", "R"),
+                   "7" => array("H", "B", "T", "R"),
+                   "8" => array("H", "B", "T", "R"),
+                   "9" => array("H", "B", "T", "R"),
+                   "10" => array("H", "B", "T", "R"),
+                   "K" => "B"
+    );
+
+    for($i = 1; $i <= 20; $i++) {
+        $key = array_rand($cards,1);
+        $random = $cards[$key];
+
+        if(is_array($random)){
+            $random_2 = array_shift($cards[$key]);
+
+            if(empty($cards[$key])){
+                unset($cards[$key]);
+            }
+            
+            $sql = "INSERT INTO cards_players (x, y, num, player)
+                    VALUES ($key, $random_2, $i, 'player_1')";
+            $st = $mysqli -> prepare($sql);
+            $st -> execute();
+        }
+        else {
+            $sql = "INSERT INTO cards_players (x, y, num, 'player')
+                    VALUES ($key, $cards[$key], $i, 'player_1')";
+            $st = $mysqli -> prepare($sql);
+            $st -> execute();
+        
+            unset($cards[$key]);
+        }
+    }
+
+    for($i = 1; $i <= 21; $i++) {
+        $key = array_rand($cards,1);
+        $random = $cards[$key];
+
+        if(is_array($random)){
+            $random_2 = array_shift($cards[$key]);
+
+            if(empty($cards[$key])){
+                unset($cards[$key]);
+            }
+
+            $sql = "INSERT INTO cards_players (x, y, num, 'player')
+                    VALUES ($key, $random_2, $i, 'player_2')";
+            $st = $mysqli -> prepare($sql);
+            $st -> execute();
+        }
+        else {
+            $sql = "INSERT INTO cards_players (x, y, num, 'player')
+                    VALUES ($key, $cards[$key], $i, 'player_2')";
+            $st = $mysqli -> prepare($sql);
+            $st -> execute();
+        
+            unset($cards[$key]);
+        }
+    }
+
+
+    show_cards();
+}
+
+?>
